4bd8d7d4b214b4b7b4e0196788d21419cf9aec78
simmslevy <simmslevy@gmail.com>
2015-02-13 00:13:42 -0500

    Scorebutt

--- opti_criterion.m
+++ opti_criterion.m
@@ -1,12 +1,17 @@
-function [score] = opti_criterion(joint_angles,robot,p_d,foot_pos)
+function [score] = opti_criterion(joint_angles,robot)
+global p_d COM_D joint_angle_goals
+
+
+[ Robut_New,COM_X,COM_Y ] = Robut_Maker( robot,joint_angles);
+
 
 % [robot,robot_com,~] = drc_forward_kinematics(new_robot);
 
 % UPDATE GRAPHICS
-draw3(robot);
+draw(Robut_New);
 
-p = robot.j(29).position_w;
-
-score = (p - p_d)'*(p - p_d);
+p = Robut_New.j(29).position_w;
+w = [1; 1; 1; 1; 1; 1;];
+score = w(1)*(p - p_d)'*(p - p_d) + w(2)*(COM_X-COM_D(1))^2+ w(2)*(COM_Y-COM_D(2))^2 + w(3)*(joint_angles - joint_angle_goals)'*(joint_angles - joint_angle_goals);
 
 end

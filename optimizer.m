c1776a9204e26476fecfb542386e63ba312e7573
Noah Fox <fox.noah@gmail.com>
2015-02-13 22:12:35 -0500

    orientation

--- optimizer.m
+++ optimizer.m
@@ -7,13 +7,14 @@ robot.j(1).position_w = [ 0 0 0.92712 ];
 robot.l(1).orientation = q_to_R( [ 0 0 0 1 ] );
 
 % INITIALIZE GLOBAL VARIABLES
-global p_d joint_angle_goals COM_D foot_pos
+global w con_opts h p_d joint_angle_goals COM_D foot_pos
 
 % DESIRED RIGHT WRIST POSITION & ORIENTATION
-p_d = [0 0.1 1];
+p_d = [.4 -0.4 1.2];
 % o_d = [];
 
 % DRAW ROBOT
+h = figure();
 draw(robot,p_d);
 
 % SET UP CONSTRAINTS
@@ -29,12 +30,16 @@ joint_angle_goals = (lb+ub)./2;
 
 COM_D = [0 0];
 
+con_opts = [1];
+
+w = [10; 1; 1; 1; 1; 1;];
+
 % INITIAL GUESS
 x0 = zeros(1,29);
 % x0 = rand(1,29);
 
 % OPTIMIZER OPTIONS
-options = optimoptions(@fmincon,'Display','iter','Algorithm','sqp','MaxIter',1000,'MaxFunEvals',10000,'TolCon',10^-3);
+options = optimoptions(@fmincon,'Display','iter','Algorithm','sqp','MaxIter',1000,'MaxFunEvals',10000,'TolCon',10^-5);
 
 % OPTIMIZE
 [x, fval] = fmincon(@opti_criterion,x0,[],[],[],[],lb,ub,@constraint_fun,options,robot);

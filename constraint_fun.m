3b441904124b67e785566bdf5dd19949e8ed8c35
Noah Fox <fox.noah@gmail.com>
2015-02-15 15:45:27 -0500

    fmincon pretty good

--- constraint_fun.m
+++ constraint_fun.m
@@ -6,12 +6,12 @@ global con_opts foot_pos p_d
 
 % GET AS CLOSE AS POSSIBLE TO TARGET
 
-eq_violations(1) = foot_pos(1,1) - robut.j(10).position_w(1);
-eq_violations(2) = foot_pos(1,2) - robut.j(10).position_w(2);
-eq_violations(3) = foot_pos(1,3) - robut.j(10).position_w(3);
-eq_violations(4) = foot_pos(2,1) - robut.j(16).position_w(1);
-eq_violations(5) = foot_pos(2,2) - robut.j(16).position_w(2);
-eq_violations(6) = foot_pos(2,3) - robut.j(16).position_w(3);
+eq_violations(1) = foot_pos(1,1) - robut.j(11).position_w(1);
+eq_violations(2) = foot_pos(1,2) - robut.j(11).position_w(2);
+eq_violations(3) = foot_pos(1,3) - robut.j(11).position_w(3);
+eq_violations(4) = foot_pos(2,1) - robut.j(17).position_w(1);
+eq_violations(5) = foot_pos(2,2) - robut.j(17).position_w(2);
+eq_violations(6) = foot_pos(2,3) - robut.j(17).position_w(3);
 
 if con_opts(1) ~= 0
     eq_violations(7) = robut.j(29).position_w(1) - p_d(1);
@@ -19,7 +19,6 @@ if con_opts(1) ~= 0
     eq_violations(9) = robut.j(29).position_w(3) - p_d(3);
 end
 
-% eq_violations
 % STAY UP
 ineq_violations = [COM_X-(0.302/2), COM_Y-(0.262/2)];
 
--- optimizer.m
+++ optimizer.m
@@ -1,4 +1,4 @@
-function [x, fval, exitflag, h] = optimizer(x0,desired_pos,opts)
+function [x, fval, exitflag, h] = optimizer(x0,weights,desired_pos,desired_orientation,opts)
 % LOAD ROBOT
 drc4
 
@@ -7,37 +7,36 @@ robot.j(1).position_w = [ 0 0 0.92712 ];
 robot.l(1).orientation = q_to_R( [ 0 0 0 1 ] );
 
 % INITIALIZE GLOBAL VARIABLES
-global w con_opts h p_d joint_angle_goals COM_D foot_pos
+global h w con_opts p_d o_d joint_angle_goals COM_D foot_pos
 
 % DESIRED RIGHT WRIST POSITION & ORIENTATION
-% p_d = [.4 -0.4 1.2];
 p_d = desired_pos;
+o_d = desired_orientation;
 
 % DRAW ROBOT
 h = figure();
 draw(robot,p_d);
 
-% SET UP CONSTRAINTS
+% JOINT LIMITS
 for i = 1:1:29
     lb(i) = robot.j(i).angle_limits(1);
     ub(i) = robot.j(i).angle_limits(2);
 end
 
+joint_angle_goals = (lb+ub)./2;
+
+% REQUIRED FOOT POSITIONS
 foot_pos = [robot.j(10).position_w;
             robot.j(16).position_w];
         
-joint_angle_goals = (lb+ub)./2;
-
+% DESIRED COM
 COM_D = [0 0];
 
-% con_opts = [1];
+% REQUIRE OR DESIRE REACHING GOAL
 con_opts = opts;
 
-w = [10; 1; 1; 1; 1; 1;];
-
-% INITIAL GUESS
-% x0 = zeros(1,29);
-% x0 = rand(1,29);
+% SCORE FUNCTION WEIGHTS
+w = weights;
 
 % OPTIMIZER OPTIONS
 options = optimoptions(@fmincon,'Display','iter','Algorithm','sqp','MaxIter',1000,'MaxFunEvals',10000,'TolCon',10^-5);

5930bca613b28dbb88b7e3a8e5d69a64f7eab53a
Noah Fox <fox.noah@gmail.com>
2015-02-15 14:17:56 -0500

    ignores figure files

--- main.m
+++ main.m
@@ -3,25 +3,25 @@ clear all; close all; clc
 % OPTIMIZATION CONDITIONS
 % p_d = [.4 -0.4 1.2]; % DESIRED POSITION
 p_d = rand(1,3)-0.5;
-N = 2; % NUMBER OF TRIALS
-rerun = 0;
+N = 100; % NUMBER OF TRIALS
 
 for i = 1:1:N
     % SET INITIAL CONDITIONS
-    if rerun ~= 1
-        con_opts = [1];
-        x0 = rand(1,29)*pi/2; % STARTING ANGLES
+    if mod(i,100) == 0
+        p_d = rand(1,3)-0.5;
     end
+    con_opts = [1];
+    x0 = rand(1,29)*pi/2; % STARTING ANGLES
     [x,score,flag,h] = optimizer(x0,p_d,con_opts);
     fig_name = sprintf('./Results/Run %i.fig',i);
     data_name = sprintf('./Results/Run %i',i);
-    if flag == -2
-        con_opts = [0];
-        i = i-1;
-        rerun = 1;
-        continue
-    end
     saveas(h,fig_name);
     save(data_name);
-    rerun = 0;
+    if flag == -2
+        con_opts = [0];
+        [x,score,flag,h] = optimizer(x0,p_d,con_opts);
+        fig_name = sprintf('./Results/Run %i.fig',i);
+        data_name = sprintf('./Results/Run %i',i);
+    end
+    close all;
 end

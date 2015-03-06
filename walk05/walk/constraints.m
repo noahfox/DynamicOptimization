7c02e877a5367b9cf56668b9136aff401dcc5427
Noah Fox <fox.noah@gmail.com>
2015-03-05 23:49:29 -0500

    ....

--- walk05/walk/constraints.m
+++ walk05/walk/constraints.m
@@ -1,10 +1,8 @@
 function [ ineq,eq ] = constraints( x )
 write_params(x);
-[out,cmdout] = system('simulate p0');
+[~,cmdout] = system('simulate p0');
 [~,crash ] = outputreader( cmdout );
 ineq = 0;
 eq = crash;
-
-
 end
 
--- walk05/walk/fminny.m
+++ walk05/walk/fminny.m
@@ -2,14 +2,16 @@ clc
 clear
 clf
 close all
+echo on
 
 % FMIN PARTY
-xin = [.89, -0.047, 0.33, -0.37, -0.10, -0.43, 0.29, 0.26,...
-              -0.080, 0.11, -0.14, -0.22, -0.031, 0.28, 0.085, 0.41,...
-              -0.38, -0.046, -0.29, -0.36, -0.12];
+% xin = [.89, -0.047, 0.33, -0.37, -0.10, -0.43, 0.29, 0.26,...
+%               -0.080, 0.11, -0.14, -0.22, -0.031, 0.28, 0.085, 0.41,...
+%               -0.38, -0.046, -0.29, -0.36, -0.12]; 
+xin = rand(21,1);
 write_params(xin);  
 
-options = optimoptions(@fmincon,'Display','iter','Algorithm','sqp','MaxIter',1000,'MaxFunEvals',10000,'TolCon',10^-5);
+options = optimoptions(@fmincon,'Display','iter','Algorithm','sqp','MaxIter',10000,'MaxFunEvals',100000,'TolCon',10^-5,'TolX',10^-100);
 
 pout = fmincon(@score,xin,[],[],[],[],[],[],@constraints,options);
 system('simulate p0')

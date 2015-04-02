9fc351fe93a93a3f4d723437eb069095da61f1ed
Noah Fox <fox.noah@gmail.com>
2015-04-01 21:24:43 -0400

    commented

--- cost_fun.m
+++ cost_fun.m
@@ -23,15 +23,15 @@ y=p(i_y0:i_y0+N);
 u_x=p(i_u_x0:i_u_x0+N);
 u_y=p(i_u_y0:i_u_y0+N);
 
-score = 0;
+score = 0; % initialize score
 
 step = 1;
 for i = 1:N-1
     if dt*i >= time(step)
         step = step+1;
     end
-    xd = (x(i+1+1)-x(i))/(2*dt);
-    yd = (y(i+1+1)-y(i))/(2*dt);
+    xd = (x(i+1+1)-x(i))/(2*dt); % COM velocity in x
+    yd = (y(i+1+1)-y(i))/(2*dt); % COM velocity in y
     score = score + (x(i) - p_x(step))^2 + xd^2 + 30*u_x(i)^2 + (y(i) - p_y(step))^2 + yd^2 + 30*u_y(i)^2;
 end
 end
--- part_1.m
+++ part_1.m
@@ -9,23 +9,27 @@ plan = 1;
 rng('shuffle','twister')
 options = optimset('MaxFunEvals',1000000,'Algorithm','sqp','Display','iter','TolFun',1e-3);
 
-
-
-
+% ----------------------------------------------------------------------- %
+% INITIALIZE OPTIMIZATION PARAMETERS
+% ----------------------------------------------------------------------- %
 % p0 is the intitial parameter vector
-x0 = rand(1,N+1);
-y0 = rand(1,N+1);
-u_x0 = zeros(1,N+1);
-u_y0 = zeros(1,N+1);
-p0=[x0 y0 u_x0 u_y0]; % a0 u0
+x0 = rand(1,N+1); % COM X
+y0 = rand(1,N+1); % COM Y
+u_x0 = zeros(1,N+1); % COP X
+u_y0 = zeros(1,N+1); % COP Y
+p0=[x0 y0 u_x0 u_y0]; % optimization vector
 
-
+% ----------------------------------------------------------------------- %
+% OPTIMIZE
+% ----------------------------------------------------------------------- %
 [answer,fval,exitflag]=fmincon(@cost_fun,p0,[],[],[],[],[],[],@constraint_fun,options);
-
 fval
 exitflag
 
-
+% ----------------------------------------------------------------------- %
+% PROCESS RESULTS
+% ----------------------------------------------------------------------- %
+% indecies for different parameter sets
 i_x0 = 1;
 i_y0 = i_x0 + N + 1;
 i_u_x0 = i_y0 + N + 1;
@@ -33,8 +37,8 @@ i_u_y0 = i_u_x0 + N + 1;
 
 
 walk_plan = read_plan(plan); % read walking plan from file
-p_x = walk_plan.p_x;
-p_y = walk_plan.p_y;
+p_x = walk_plan.p_x; % x foot locations
+p_y = walk_plan.p_y; % y foot locations
 stance = walk_plan.stance_type;
 step_dur = walk_plan.duration;
 time = walk_plan.time;
@@ -46,8 +50,8 @@ for i = 1:1:N-1
     if dt*i >= time(step)
         step = step+1;
     end
-    p_traj_x(i) = p_x(step);
-    p_traj_y(i) = p_y(step);
+    p_traj_x(i) = p_x(step); % make trajectory of steps in x
+    p_traj_y(i) = p_y(step); % make trajectory of steps in y
 end
 
 fig(1) = figure();

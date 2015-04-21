a3fdace500e50aab713ccedbdcaeb41865c95290
Noah Fox <fox.noah@gmail.com>
2015-04-20 22:30:37 -0400

    ..

--- LQR/lqrmaker.m
+++ LQR/lqrmaker.m
@@ -4,24 +4,24 @@ global target
 syms y z th ps dy dz dth dpsi m ft T real
 
 f_vec = [   dy
-    dz
-    dth
-    dpsi
-    0
-    -consts.g
-    0
-    0
-    0] ;
+            dz
+            dth
+            dpsi
+            0
+            -consts.g
+            0
+            0
+            0] ;
 
 g_vec = [0,    0 ;
-    0,    0 ;
-    0,    0 ;
-    0,    0 ;
-    -consts.gamma*sin(ps+th)/m,    0 ;
-    consts.gamma*cos(ps+th)/m,    0 ;
-    -consts.L*consts.gamma*sin(ps)/consts.J,    0 ;
-    0, 1/consts.JT ;
-    -1,    0] ;
+        0,    0 ;
+        0,    0 ;
+        0,    0 ;
+        -consts.gamma*sin(ps+th)/m,    0 ;
+        consts.gamma*cos(ps+th)/m,    0 ;
+        -consts.L*consts.gamma*sin(ps)/consts.J,    0 ;
+        0, 1/consts.JT ;
+        -1,    0] ;
 
 x = [y z th ps dy dz dth dpsi m];
 u = [ft T];
